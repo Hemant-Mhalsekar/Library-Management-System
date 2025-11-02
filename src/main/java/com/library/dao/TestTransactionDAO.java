da6cab14ccf942924d955b5b3545c76d2c4520a0
Hemant-Mhalsekar <hemantmhalsekar@gmail.com>
2025-11-03 00:59:44 +0530

    Completed TransactionDAO

---
+++ src/main/java/com/library/dao/TestTransactionDAO.java
@@ -0,0 +1,31 @@
+package com.library.dao;
+
+import com.library.model.Transaction;
+import java.time.LocalDate;
+import java.util.List;
+
+public class TestTransactionDAO {
+    public static void main(String[] args) {
+        TransactionDAO dao = new TransactionDAO();
+
+        //Add new transaction (Book issued)
+        Transaction t1 = new Transaction(1, 101, 201, LocalDate.now(), null, 0.0);
+        dao.addTransaction(t1);
+
+        //Get all transactions
+        List<Transaction> transactions = dao.getAllTransactions();
+        for (Transaction t : transactions) {
+            System.out.println(t);
+        }
+
+        //Get transaction by ID
+        Transaction found = dao.getTransactionById(1);
+        System.out.println(found != null ? found : "No transaction found!");
+
+        //Update return date and fine
+        dao.updateReturnDateAndFine(1, LocalDate.now().plusDays(10), 15.0);
+
+        //Delete transaction
+        dao.deleteTransaction(1);
+    }
+}
---
+++ src/main/java/com/library/dao/TransactionDAO.java
@@ -0,0 +1,132 @@
+package com.library.dao;
+
+import com.library.model.Transaction;
+
+import java.sql.*;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TransactionDAO {
+
+    public void addTransaction(Transaction transaction){
+        String sql = "INSERT INTO transactions (transaction_id, book_id, member_id, issue_date, return_date, fine_amount) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, transaction.getTransactionId());
+            stmt.setInt(2, transaction.getBookId());
+            stmt.setInt(3, transaction.getMemberId());
+            stmt.setDate(4, java.sql.Date.valueOf(transaction.getIssueDate()));
+
+            if (transaction.getReturnDate() != null){
+                stmt.setDate(5, java.sql.Date.valueOf(transaction.getReturnDate()));
+            } else {
+                stmt.setNull(5, Types.DATE);
+            }
+
+            stmt.setDouble(6, transaction.getFineAmount());
+
+            int rowInserted = stmt.executeUpdate();
+            if (rowInserted > 0) {
+                System.out.println("Transaction added successfully for Book ID: " + transaction.getBookId());
+            }
+        } catch (SQLException e) {
+            System.out.println("Error adding transaction: " +e.getMessage());
+        }
+    }
+
+    public List<Transaction> getAllTransactions() {
+        List<Transaction> transactions = new ArrayList<>();
+        String sql = "SELECT * FROM transactions";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql);
+             ResultSet rs = stmt.executeQuery()) {
+
+            while (rs.next()) {
+                Transaction transaction = new Transaction(
+                        rs.getInt("transaction_id"),
+                        rs.getInt("book_id"),
+                        rs.getInt("member_id"),
+                        rs.getDate("issue_date").toLocalDate(),
+                        rs.getDate("return_date") != null ? rs.getDate("return_date").toLocalDate() : null,
+                        rs.getDouble("fine_amount")
+                );
+                transactions.add(transaction);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching transactions: " + e.getMessage());
+        }
+        return  transactions;
+    }
+
+    public Transaction getTransactionById(int transactionId) {
+        Transaction transaction = null;
+        String sql = "SELECT * FROM transactions WHERE transaction_id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+             PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setInt(1, transactionId);
+            ResultSet rs = stmt.executeQuery();
+
+            if (rs.next()) {
+                transaction = new Transaction(
+                        rs.getInt("transaction_id"),
+                        rs.getInt("book_id"),
+                        rs.getInt("member_id"),
+                        rs.getDate("issue_date").toLocalDate(),
+                        rs.getDate("return_date") != null ? rs.getDate("return_date").toLocalDate() : null,
+                        rs.getDouble("fine_amount")
+                );
+            }
+        } catch (SQLException e) {
+            System.out.println("Error fetching transaction by ID: " + e.getMessage());
+        }
+        return transaction;
+    }
+
+    public void updateReturnDateAndFine(int transactionId, LocalDate returnDate, double fineAmount) {
+        String sql = "UPDATE transactions SET return_date = ?, fine_amount = ? WHERE transaction_id = ?";
+
+        try (Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)){
+
+            stmt.setDate(1, java.sql.Date.valueOf(returnDate));
+            stmt.setDouble(2, fineAmount);
+            stmt.setInt(3, transactionId);
+
+            int rowUpdated = stmt.executeUpdate();
+
+            if (rowUpdated > 0) {
+                System.out.println("Return date and fine updated for transaction ID: " + transactionId);
+            } else {
+                System.out.println("No transaction found with ID: " + transactionId);
+            }
+
+        } catch (SQLException e) {
+            System.out.println("Error updating transaction: " + e.getMessage());
+        }
+    }
+
+    public void deleteTransaction(int transactionId) {
+        String sql = "DELETE FROM transactions WHERE transaction_id = ?";
+
+        try(Connection conn = DatabaseConnection.getConnection();
+            PreparedStatement stmt = conn.prepareStatement(sql)) {
+
+            stmt.setInt(1, transactionId);
+            int rowDeleted = stmt.executeUpdate();
+
+            if (rowDeleted > 0) {
+                System.out.println("Transaction with ID " + transactionId + " deleted successfuly!");
+            } else {
+                System.out.println("No transaction found with ID: " + transactionId);
+            }
+        } catch (SQLException e) {
+            System.out.println("Error deleting transaction: "+ e.getMessage());
+        }
+    }
+}
